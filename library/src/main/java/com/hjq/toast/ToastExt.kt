b85f7cff4b8f09da0d52a2fb378f852cbc129fe0
TimChen <RSTimChen@gmail.com>
2020-04-09 19:32:55 +0800

    Add failOnError false

--- library/src/main/java/com/hjq/toast/ToastExt.kt
+++ library/src/main/java/com/hjq/toast/ToastExt.kt
@@ -1,13 +1,13 @@
 package com.hjq.toast
 
+import android.app.Activity
+import android.content.Context
+
 /**
  *  author: Tim Chen
  *  time  : 2020-04-09
  *  desc  :
  */
-import android.app.Activity
-import android.content.Context
-
 inline fun Activity.toastMe(message: CharSequence) = ToastUtils.show(message)
 
 inline fun Context.toastMe(message: CharSequence) = ToastUtils.show(message)
